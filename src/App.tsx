5119af5c6eef516e76dc75a6beb07e383d96f330
Jesus Ruelas-Perez <91504943+Jesus711@users.noreply.github.com>
2023-07-06 20:32:17 -0700

    Created RepoItem Component that will be used to display repo's information and Added Typing to all Repo's props

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 import { Octokit } from '@octokit/core';
+import RepoItem from './components/RepoItem';
 
 
 function App() {
   window.global ||= window;
   const [searched, setSearched] = useState("");
+  const [saveSearch, setSaveSearch] = useState("");
+  const [repos, setRepos] = useState([])
 
   const token = import.meta.env.VITE_GITHUB_API_TOKEN;
   const octokit = new Octokit( {auth: `${token}` });
@@ -17,8 +20,28 @@ function App() {
 
   async function handleFormSubmit(e: any) {
     e.preventDefault()
-    console.log(searched)
+
+    setSearched("")
+    setSaveSearch(searched)
     let search_result = await octokit.request(`GET /orgs/${searched}/repos`)
+    .then(res => {
+      if(res.status >= 400){
+        return null;
+      }
+
+      return res.data
+    }).catch(err => {
+      console.log(err);
+    })
+
+    if(search_result === null){
+      setRepos([])
+      return;
+    }
+
+
+    setRepos(search_result)
+
     console.log(search_result)
   }
 
@@ -31,8 +54,17 @@ function App() {
         <input id='search-input' type="search" placeholder='Enter Org Name' value={searched} onChange={(e) => {setSearched(e.target.value)}}></input>
         <button id='search-btn' type='submit'>Search</button>
       </form>
-      <div className='results'>
-        <h2>Search Results:</h2>
+      <div className='results-section'>
+        <h2>{`'${saveSearch}'`} Search Results:</h2>
+        <div className='repo-list'>
+          {repos.length > 0 && repos.map(repo => {
+            return (
+              <RepoItem repo={repo}/>
+            )
+          })}
+
+
+        </div>
       </div>
     </>
   )
