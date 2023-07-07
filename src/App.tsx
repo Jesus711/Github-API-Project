61f17705305cdb1c0c682b22347ed17d1bda4acc
Jesus Ruelas-Perez <91504943+Jesus711@users.noreply.github.com>
2023-07-06 19:17:41 -0700

    Started Mobile First Design creating a simple Form to Search for Organizations Repos

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,39 @@
-import { useState } from 'react'
-import './App.css'
+import { useEffect, useState } from 'react';
+import './App.css';
+import { Octokit } from 'octokit';
+
 
 function App() {
+  window.global ||= window;
+  const [searched, setSearched] = useState("");
 
+  const token = import.meta.env.VITE_GITHUB_API_TOKEN;
+  const octokit = new Octokit( {auth: `${token}` });
+
+  useEffect(() => {
+
+  }, [])
+
+
+  async function handleFormSubmit(e: any) {
+    e.preventDefault()
+    console.log(searched)
+    let search_result = await octokit.request(`GET /orgs/${searched}/repos`)
+    console.log(search_result)
+  }
 
 
   return (
     <>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1 className='title'>RepOrg Search</h1>
+      <p className='project-desc'>Search Popular Organizations Public GitHub Repositories</p>
+      <form className='search-repo-form' onSubmit={(e) => {handleFormSubmit(e)}}>
+        <input id='search-input' type="search" placeholder='Enter Org Name' value={searched} onChange={(e) => {setSearched(e.target.value)}}></input>
+        <button id='search-btn' type='submit'>Search</button>
+      </form>
+      <div className='results'>
+        <h2>Search Results:</h2>
+      </div>
     </>
   )
 }
