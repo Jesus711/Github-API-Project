4b74f249e4918471df08a7d53243257296322529
Jesus Ruelas-Perez <91504943+Jesus711@users.noreply.github.com>
2023-07-14 17:34:20 -0700

    Added Function to Sort Repo Results by Stargazers_count in descending order

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,6 +3,90 @@ import { Octokit } from '@octokit/core';
 import RepoItem from './RepoItem';
 
 
+interface Repo {
+  id: number,
+  name: string,
+  node_id : string,
+  full_name : string,
+  private : boolean,
+  owner : object,
+  html_url : string,
+  description : string,
+  fork : boolean,
+  url : string,
+  forks_url : string,
+  keys_url : string,
+  collaborators_url : string,
+  teams_url : string,
+  hooks_url : string,
+  issue_events_url : string,
+  events_url : string,
+  assignees_url : string,
+  branches_url : string,
+  tags_url : string,
+  blobs_url : string,
+  git_tags_url : string,
+  git_refs_url : string,
+  trees_url : string,
+  statuses_url : string,
+  languages_url : string,
+  stargazers_url : string,
+  contributors_url : string,
+  subscribers_url : string,
+  subscription_url : string,
+  commits_url : string,
+  git_commits_url : string,
+  comments_url : string,
+  issue_comment_url : string,
+  contents_url : string,
+  compare_url : string,
+  merges_url : string,
+  archive_url : string,
+  downloads_url : string,
+  issues_url : string,
+  pulls_url : string,
+  milestones_url : string,
+  notifications_url : string,
+  labels_url : string,
+  releases_url : string,
+  deployments_url : string,
+  created_at : string,
+  updated_at : string,
+  pushed_at : string,
+  git_url : string,
+  ssh_url : string,
+  clone_url : string,
+  svn_url : string,
+  homepage : string,
+  size : number,
+  stargazers_count : number,
+  watchers_count : number,
+  language : string,
+  has_issues : boolean,
+  has_projects : boolean,
+  has_downloads : boolean,
+  has_wiki : boolean,
+  has_pages : boolean,
+  has_discussions : boolean,
+  forks_count : number,
+  mirror_url : object,
+  archived : boolean,
+  disabled : boolean,
+  open_issues_count : number,
+  license : object,
+  allow_forking : boolean,
+  is_template : boolean,
+  web_commit_signoff_required : boolean,
+  topics : object,
+  visibility : string,
+  forks : number,
+  open_issues : number,
+  watchers : number,
+  default_branch : string,
+  permissions : object,
+
+}
+
 function Home() {
   window.global ||= window;
   const [searched, setSearched] = useState(() => {
@@ -31,7 +115,8 @@ function Home() {
 
 
   useEffect(() => {
-      handleFormSubmit(null)
+      if(searched !== "")
+        handleFormSubmit(null)
   }, [])
 
 
@@ -88,13 +173,30 @@ function Home() {
       return;
     }
 
+    sortRepos(search_result);
+
     result_none.style.display = "none";
     header[1].style.display = 'block';
     //loader.style.display = "none";
     window.sessionStorage.setItem('results', JSON.stringify(search_result));
     setRepos(search_result)
 
-    console.log(search_result)
+    //console.log(search_result)
+  }
+
+
+  function sortRepos(reposList: Array<Repo>): void {
+    reposList.sort((a, b) => {
+      if(a.stargazers_count > b.stargazers_count){
+        return -1;
+      }
+
+      if (a.stargazers_count < b.stargazers_count) {
+        return 1;
+      }
+
+      return 0;
+    })
   }
 
 
